cb353faecf581cfb36750f610f3baff258255d42
Bria White <bria.awhite@gmail.com>
2020-07-29 01:47:45 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,18 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot
+  puts "Squawk!"
+end  
+
+def print_parrot
+  phrase="Squawk!"
+  "Squawk!"
+end  
+
+def parrot(phrase="Squawk!")
+  puts phrase
+end
+
+def parrot(phrase=parrot)
+  puts "Pretty Bird!"
+end  
